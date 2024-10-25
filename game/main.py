5a36b518e609ace26ee2b52692e8fb76855f65a3
Hélisson Magalhães <helissonmagalhaes@hotmail.com>
2024-10-25 03:58:01 +0100

    Adding sprites

--- game/main.py
+++ game/main.py
@@ -6,6 +6,7 @@ import websocket
 import json
 import time
 import uuid
+import spritesheet
 
 
 # Constants
@@ -18,6 +19,7 @@ FRAMES_PER_SECOND = 60
 
 # I added this normalizer because i want to all velocity be visually the same no matter the FPS
 NORMALIZER = 60 / FRAMES_PER_SECOND
+TOGGLE_HITBOX = False
 
 # Initialize Pygame
 pygame.init()
@@ -31,6 +33,10 @@ SPAWN_RECT_EVENT = pygame.USEREVENT + 1
 CAN_SPAWN_PARTICLE = None
 pygame.time.set_timer(SPAWN_RECT_EVENT, 17)  # milliseconds
 
+# Ssprite
+sprite_sheet_image = pygame.image.load('doux.png').convert_alpha()
+sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
+
 
 class Particle:
     def __init__(self, position):
@@ -41,7 +47,7 @@ class Particle:
         self.velocity = [random.uniform(-1, 1), random.uniform(2, 5)]
         self.surface = pygame.Surface((self.size, self.size))
         self.fadeout_alpha = 255
-        self.fadeout_rate = 120 / FRAMES_PER_SECOND
+        self.fadeout_rate = 900 / FRAMES_PER_SECOND
 
     def update(self):
         self.lifetime -= 1 * NORMALIZER
@@ -54,29 +60,82 @@ class Particle:
         screen.blit(self.surface, (self.position[0], self.position[1]))
 
 
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, x: float, y: float, name: str):
+        super().__init__()
         self.pos_x = x
         self.pos_y = y
         self.name = name
         self.color = (255, 0, 0)  # Default color red
-        self.size = 50.0
         self.speed = 5
-        self.weapon_range = self.size * 1.05
+
         self.particles = []
         self.last_position = None
+
+        self.sprites_stopped = []
+        self.sprites_stopped.append((sprite_sheet.get_image(0, 24, 24, 3, BLACK)))
+        self.sprites_stopped.append((sprite_sheet.get_image(1, 24, 24, 3, BLACK)))
+        self.sprites_stopped.append((sprite_sheet.get_image(2, 24, 24, 3, BLACK)))
+        self.sprites_stopped.append((sprite_sheet.get_image(3, 24, 24, 3, BLACK)))
+
+        self.sprites_walking = []
+        self.sprites_walking.append((sprite_sheet.get_image(6, 24, 24, 3, BLACK)))
+        self.sprites_walking.append((sprite_sheet.get_image(7, 24, 24, 3, BLACK)))
+        self.sprites_walking.append((sprite_sheet.get_image(8, 24, 24, 3, BLACK)))
+        self.sprites_walking.append((sprite_sheet.get_image(9, 24, 24, 3, BLACK)))
+
+        self.current_sprite = 0
+        self.image = self.sprites_stopped[self.current_sprite]
+        self.rect = self.image.get_rect()
+        self.hitbox = self.image.get_bounding_rect(min_alpha=1)
+        self.weapon_range = self.hitbox.height * 1.05
+        self.rect.topleft = [self.pos_x, self.pos_y]
+
+        self.is_walking_right = False
+        self.is_walking_left = False
+
+        ## Other player
+        self.last_time_walking = 0
+
+    @property
+    def hitbox_position(self):
+        x_readjustiment = (self.rect.width - self.hitbox.width) // 2
+        y_readjustiment = (self.rect.height - self.hitbox.height) // 2
+        return [self.pos_x + x_readjustiment, self.pos_y + y_readjustiment + 1]
 
     def move(self, keys):
         """Handle player movement based on arrow key input."""
         pos_x, pos_y = self.pos_x, self.pos_y
         if keys[pygame.K_LEFT]:
-            pos_x = max(0, self.pos_x - self.speed * NORMALIZER)
+            if self.hitbox.x > self.speed:
+                self.is_walking_left = True
+                pos_x = self.pos_x - self.speed
+            elif self.hitbox.x > 0:
+                self.is_walking_left = False
+                pos_x = self.pos_x - self.hitbox.x
+        else:
+            self.is_walking_left = False
+
         if keys[pygame.K_RIGHT]:
-            pos_x = min(SCREEN_WIDTH - self.size, self.pos_x + self.speed * NORMALIZER)
+            if self.hitbox.x + self.hitbox.width + self.speed < SCREEN_WIDTH:
+                self.is_walking_right = True
+                pos_x = self.pos_x + self.speed
+            elif self.hitbox.x + self.hitbox.width < SCREEN_WIDTH:
+                self.is_walking_right = False
+                pos_x = self.pos_x + SCREEN_WIDTH - (self.hitbox.x + self.hitbox.width)
+        else:
+            self.is_walking_right = False
+
         if keys[pygame.K_UP]:
-            pos_y = max(0, self.pos_y - self.speed * NORMALIZER)
+            if self.hitbox.y > self.speed:
+                pos_y = self.pos_y - self.speed
+            elif self.hitbox.y > 0:
+                pos_y = self.pos_y - self.hitbox.y
         if keys[pygame.K_DOWN]:
-            pos_y = min(SCREEN_HEIGHT - self.size, self.pos_y + self.speed * NORMALIZER)
+            if self.hitbox.y + self.hitbox.height + self.speed < SCREEN_HEIGHT:
+                pos_y = self.pos_y + self.speed
+            elif self.hitbox.y + self.hitbox.height < SCREEN_HEIGHT:
+                pos_y = self.pos_y + SCREEN_HEIGHT - (self.hitbox.y + self.hitbox.height)
 
         self.update_position(pos_x, pos_y)
 
@@ -85,19 +144,36 @@ class Player:
         # The FPS
         global CAN_SPAWN_PARTICLE
         self.last_position = (self.pos_x, self.pos_y)
+
         if self.last_position != (pos_x, pos_y) and CAN_SPAWN_PARTICLE == SPAWN_RECT_EVENT:
-            self.particles.append(Particle([pos_x + self.size / 2, pos_y + self.size]))
+            self.particles.append(Particle([self.hitbox.x + self.hitbox.width / 2, self.hitbox.y + self.hitbox.height]))
+
         self.pos_x, self.pos_y = pos_x, pos_y
+        self.rect.topleft = [self.pos_x, self.pos_y]
+        self.hitbox.topleft = self.hitbox_position
 
     @property
     def center(self):
-        return (self.pos_x + self.size / 2, self.pos_y + self.size / 2)
+        return (self.hitbox.x + self.hitbox.width / 2, self.hitbox.y + self.hitbox.height / 1.5)
+
+    def update_sprite(self):
+        self.current_sprite += 10 / FRAMES_PER_SECOND
+        sprites = self.sprites_stopped
+        if self.is_walking_left or self.is_walking_right or (self != local_player and pygame.time.get_ticks() - self.last_time_walking < 100):
+            sprites = self.sprites_walking
+
+        if self.current_sprite >= len(sprites):
+            self.current_sprite = 0
+        self.image = sprites[int(self.current_sprite)]
 
     def draw(self, screen):
         """Draw the player square and name on the screen."""
-        pygame.draw.rect(screen, self.color, (self.pos_x, self.pos_y, self.size, self.size))
+        if TOGGLE_HITBOX:
+            pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 1)
+        screen.blit(self.image, self.rect)
+
         text_surface = font.render(self.name, True, BLACK)  # Black text
-        screen.blit(text_surface, (self.pos_x + self.size + 10, self.pos_y))  # Display name next to the square
+        screen.blit(text_surface, (self.pos_x + self.hitbox.width + 10, self.pos_y))
         if self == local_player:
             self._draw_weapon_range(screen)
 
@@ -141,11 +217,15 @@ def receive_player_positions(ws):
     while True:
         message = ws.recv()
         players_data = json.loads(message)
+        print(players_data)
         for player_data in players_data.values():
             name = player_data["name"]
             if name not in all_players:
                 all_players[name] = Player(x=player_data["x"], y=player_data["y"], name=name)
             else:
+                if all_players[name].last_position != (player_data["x"], player_data["y"]):
+                    all_players[name].last_time_walking = pygame.time.get_ticks()
+
                 all_players[name].update_position(player_data["x"], player_data["y"])
 
 
@@ -158,7 +238,9 @@ def start_multiplayer_game():
     # Start threads for sending and receiving positions
     threading.Thread(target=send_player_position, args=(ws,), daemon=True).start()
     threading.Thread(target=receive_player_positions, args=(ws,), daemon=True).start()
-    global CAN_SPAWN_PARTICLE
+    global CAN_SPAWN_PARTICLE, TOGGLE_HITBOX
+    toggle_hitbox_cool_down = 500  # millisecond
+    toggle_hitbox_lasttime_clicked = 0
     running = True
     while running:
         CAN_SPAWN_PARTICLE = None
@@ -169,12 +251,18 @@ def start_multiplayer_game():
                 CAN_SPAWN_PARTICLE = event.type
 
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            current_time = pygame.time.get_ticks()
+            if current_time - toggle_hitbox_lasttime_clicked > toggle_hitbox_cool_down:
+                TOGGLE_HITBOX = not TOGGLE_HITBOX
+                toggle_hitbox_lasttime_clicked = current_time
+
         local_player.move(keys)
 
         screen.fill(WHITE)
-
         # Draw all players
         for player in all_players.values():
+            player.update_sprite()
             player.draw(screen)
             # Update and draw particles
             for particle in player.particles:
